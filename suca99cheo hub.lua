58b016404079fd57e01b54e5ee68e188a49a8022
cholacoho123 <cholacoho123@gmail.com>
2025-11-02 17:21:59 +0700

    Update suca99cheo hub.lua

--- suca99cheo hub.lua
+++ suca99cheo hub.lua
@@ -2,10 +2,10 @@ getgenv().Config = {
     DPS_THRESHOLDS = {
         HOUSE1 = { '>0' },
         HOUSE2 = { '>1000' },
-        HOUSE3 = { '6000~14000', '>25000' },
-        HOUSE4 = { '14000~20000' },
-        HOUSE5 = { '>20000' },
-        HOUSE6 = { '>26000' }, 
+        HOUSE3 = { '6000~14000', '>14000' },
+        HOUSE4 = { '14000~20000' '>23000' },
+        HOUSE5 = { '>23000' },
+        HOUSE6 = { '>26000' },
     },
     plant = {
         PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot đặt pet, put pet
