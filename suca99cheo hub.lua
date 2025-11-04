8eaf8f84f5912ec633336bdfed8c32f29dccf474
cholacoho123 <cholacoho123@gmail.com>
2025-11-04 17:12:02 +0700

    Update suca99cheo hub.lua

--- suca99cheo hub.lua
+++ suca99cheo hub.lua
@@ -29,4 +29,5 @@ getgenv().Config = {
         ['Exclusive Egg 56'] = true, --- SEND Exclusive Egg
     },
 }
+script_key="PeYHdVDZHyteYIWLngzUBVQRfRHGPTLJ";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b24916c869a061ce85451e63930edda.lua"))()
