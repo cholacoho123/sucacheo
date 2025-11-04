39f7cb2321855e56955b4145d7d68a0facae0ee9
cholacoho123 <cholacoho123@gmail.com>
2025-11-04 15:02:17 +0700

    Update suca99cheo hub.lua

--- suca99cheo hub.lua
+++ suca99cheo hub.lua
@@ -1,1 +1,32 @@
+--SOI CÁI ĐỊT MẸ MÀY
+getgenv().Config = {
+    DPS_THRESHOLDS = {
+        HOUSE1 = { '>0' },
+        HOUSE2 = { '>1000' },
+        HOUSE3 = { '6000~14000', '>14000' },
+        HOUSE4 = { '14000~20000', '>23000' },
+        HOUSE5 = { '>23000' },
+        HOUSE6 = { '>26000' },
+    },
+    plant = {
+        PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot đặt pet, put pet
+        EGG_SLOTS = { 10 }, -- slot dành cho trứng -- slot đặt trứng , put egg
+    },
+    SEND_PET = {
+        Usernames = { 'OKkMma_b' },  --gửi cho nhiều usename,send multi 'usename'
+        PetSendInterval = 20,
+    },
+    SEND_DIAMONDS = {
+        Usernames = { 'OKkMma_b' },--gửi cho nhiều usename,send multi usename
+        MinDiamonds = 50000000,
+    },
+    SEND_ITEM = {
+        Usernames = { 'usename' },
+        ['Spectral Potion'] = { amount = 3 },--gửi cho nhiều usename,send multi usename
+    },
+    SEND_EGGS = {
+        Usernames = { 'OKkMma_b' },
+        ['Exclusive Egg 56'] = true, --- SEND Exclusive Egg
+    },
+}
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7c082a1ad968228f9d5e0d6eb722ca19.lua"))()
