0cf05cd70d584fda0da418bc0dcea782c77ab4ca
cholacoho123 <cholacoho123@gmail.com>
2025-11-13 03:23:49 +0700

    Update succac.lua

--- succac.lua
+++ succac.lua
@@ -1,43 +1,40 @@
+script_key = 'mAnM0huowuGMYZ'
 getgenv().Config = {
-SERVER_HOP = false, -- true: bật hop server, false: tắt hop server
+    SERVER_HOP = false, -- true: bật hop server, false: tắt hop server
     DPS_THRESHOLDS = {
         HOUSE1 = { '>0' },
-        HOUSE2 = { '1000~3000', '>10000' },
-        HOUSE3 = { '3000~6500', '>16000' },
-        HOUSE4 = { '6000~9000', '>23000' },
-        HOUSE5 = { '8300~22000' },
-        HOUSE6 = { '>25000'},
+        HOUSE2 = { '>1000' },
+        HOUSE3 = { '6000~14000', '>25000' },
+        HOUSE4 = { '14000~24000', '>35000' },
+        HOUSE5 = { '>24000' },
+        HOUSE6 = { '>30000' },
     },
     plant = {
         PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot đặt pet, put pet
-        EGG_SLOTS = { 10 }, -- slot dành cho trứng -- slot đặt trứng , put egg
+        EGG_SLOTS = { 10 }, -- slot đặt trứng , put egg
     },
     SEND_PET = {
-        Usernames = { 'OKkMma_b' },
+        Usernames = { '' }, --- name1 , name2 , name3 ---
         PetSendInterval = 20,
-        SEND_ALL = true, -- true: gửi tất cả pet Configsendsend, false: chỉ gửi Name_Pet
-        Name_Pet = {
-            'Huge Evil Raven',
-            'Huge Skeleton Snake',
-            'Huge Vampire Agony',
-        },
+        SEND_ALL = true, -- true: SEND ALL, false: SEND PET CONFIG ----
+        Name_Pet = { '' }, --HUGE NAME 1 , HUGE NAME 2 , HUGE NAME 3 ---
     },
     SEND_DIAMONDS = {
-        Usernames = { ''}, -- 'name1', 'name2', 'name3'
-        MinDiamonds = 40000000,
+        Usernames = { '' }, --- name1 , name2 , name3 ---
+        MinDiamonds = 100000000,
     },
-    SEND_ITEM = { 
-        Usernames = { ''}, ---'name1', 'name2', 'name3'
-        ['Spectral Potion'] = { amount = 3 },
+    SEND_ITEM = {
+        Usernames = { '' }, --- name1 , name2 , name3 ---
+        ['Spectral Potion'] = { amount = 3 }, --- number and 'all' ----
     },
     SEND_EGGS = {
-        Usernames = { ''}, ---'name1', 'name2', 'name3'
+        Usernames = { '' }, --- name1 , name2 , name3 ---
         ['Exclusive Egg 56'] = true,
     },
     Webhook = {
-        ID = '1254610781677289522', -- Discord ID d? tag
-        IdNames = { 'Huge', 'Titanic' }, -- Pet c?n theo dõi
-        URL = 'https://discord.com/api/webhooks/1312555300808818768/sLXhl-mfBsOu8Wy_vfzP9tpu-DIDUmLGUkHzMOKzOzvSpB0PV4-glDoP_FFpSJRVET3_', -- URL webhook private
+        ID = '', -- Discord ID để tag
+        IdNames = { 'Huge', 'Titanic' }, -- Pet cần theo dõi
+        URL = '', -- URL webhook private
     },
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7c082a1ad968228f9d5e0d6eb722ca19.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xitrumhub/sucacheo-hub/refs/heads/main/SUCACHEO%20HUB'))()
